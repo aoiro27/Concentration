11cc2289483ad2ca318d39d6d8bcdd51760e3469
aoiro27 <aoiro.savant@gmail.com>
2025-07-13 08:20:05 +0900

    init

---
+++ Concentration/ConcentrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcentrationApp.swift
+//  Concentration
+//
+//  Created by aoiro on 2025/07/13.
+//
+
+import SwiftUI
+
+@main
+struct ConcentrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
